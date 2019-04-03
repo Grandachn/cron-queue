c87786be84819231cfca5cd830ec6f824254db67
何冠达 <guanda.he@zhenai.com>
2019-04-03 19:29:54 +0800

    基于lua脚本实现redis的读写锁

--- cron-queue-core/src/main/java/io/github/grandachn/cronqueue/redis/rock/RedisReadLock.java
+++ cron-queue-core/src/main/java/io/github/grandachn/cronqueue/redis/rock/RedisReadLock.java
@@ -1,6 +1,5 @@
 package io.github.grandachn.cronqueue.redis.rock;
 
-import io.github.grandachn.cronqueue.exception.CommonException;
 import io.github.grandachn.cronqueue.redis.JedisTemplate;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,8 +12,8 @@ import java.util.concurrent.TimeUnit;
  */
 @Slf4j
 public class RedisReadLock {
-    public static void lock(String name, long leaseTime, TimeUnit unit){
-        tryLock(name, Long.MAX_VALUE, leaseTime, TimeUnit.SECONDS);
+    public static void lock(String name){
+        tryLock(name, Long.MAX_VALUE, 30, TimeUnit.SECONDS);
     }
 
     public static boolean tryLock(String name, long waitTime, long leaseTime, TimeUnit unit){
@@ -23,19 +22,25 @@ public class RedisReadLock {
         StringBuilder sctipt = new StringBuilder();
         // write-lock read-lock uuid leaseTime
         sctipt.append("if not redis.call('GET',KEYS[1]) then ")
-                .append("redis.call('HSET',KEYS[2],KEYS[3],ARGV[1]);")
-                .append("local t = redis.call('PTTL', KEYS[2]);")
-                .append("redis.call('PEXPIRE', KEYS[2], math.max(t, ARGV[1]));")
-                .append("return 1;")
+                        //若没有值，返回的是false
+                    .append("local count = redis.call('HGET',KEYS[2],KEYS[3]);")
+                    .append("if count then ")
+                        .append("count = tonumber(count) + 1;")
+                        .append("redis.call('HSET',KEYS[2],KEYS[3],count);")
+                    .append("else ")
+                        .append("redis.call('HSET',KEYS[2],KEYS[3],1);")
+                    .append("end;")
+                    .append("local t = redis.call('PTTL', KEYS[2]);")
+                    .append("redis.call('PEXPIRE', KEYS[2], math.max(t, ARGV[1]));")
+                    .append("return 1;")
                 .append("else ")
-                .append("return 0;")
+                     .append("return 0;")
                 .append("end;");
-        Long res;
         for(;;){
             if(System.currentTimeMillis() > waitUntilTime){
                 return false;
             }
-            res = (Long) JedisTemplate.operate().eval(sctipt.toString(), 3, RedisReadWriteLock.getWriteLockKey(name), RedisReadWriteLock.getReadLockKey(name), RedisReadWriteLock.getThreadUid(), leastTimeLong.toString());
+            Long res = (Long) JedisTemplate.operate().eval(sctipt.toString(), 3, RedisReadWriteLock.getWriteLockKey(name), RedisReadWriteLock.getReadLockKey(name), RedisReadWriteLock.getThreadUid(), leastTimeLong.toString());
             if(res.equals(1L)){
                 //successGetReadLock
                 log.info("success get read lock,  readLock={}", RedisReadWriteLock.getReadLockKey(name));
@@ -53,11 +58,28 @@ public class RedisReadLock {
         return true;
     }
 
-    public void unlock(){
-
+    public static void unlock(String name){
+        StringBuilder sctipt = new StringBuilder();
+        sctipt.append("local count = redis.call('HGET',KEYS[1],KEYS[2]);")
+                .append("if count then ")
+                    .append("if (tonumber(count) > 1) then ")
+                        .append("count = tonumber(count) - 1;")
+                        .append("redis.call('HSET',KEYS[1],KEYS[2],count);")
+                    .append("else ")
+                      .append("redis.call('HDEL',KEYS[1],KEYS[2]);")
+                    .append("end;")
+                .append("end;")
+                .append("return;");
+        JedisTemplate.operate().eval(sctipt.toString(), 2, RedisReadWriteLock.getReadLockKey(name), RedisReadWriteLock.getThreadUid());
+        log.info("success unlock read lock, readLock={}", RedisReadWriteLock.getReadLockKey(name));
     }
 
     public static void main(String[] args) {
         tryLock("b", 30,300, TimeUnit.SECONDS);
+        tryLock("b", 30,300, TimeUnit.SECONDS);
+        tryLock("b", 30,300, TimeUnit.SECONDS);
+        unlock("b");
+        unlock("b");
+        unlock("b");
     }
 }
